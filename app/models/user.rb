fdfc6d4a327eec80f77a55cff6879584f0048a5c
stephan-dev <stephan-dev@outlook.fr>
2018-11-05 23:54:12 +0100

    push

---
+++ app/models/user.rb
@@ -0,0 +1,27 @@
+class User < ApplicationRecord
+
+
+
+
+  #validates :first_name, presence: true, length: {maximum: 50}
+  #validates :last_name, presence: true, length: {maximum: 50}
+
+  #validates :email, presence: true, uniqueness: { case_sensitive: false }
+
+  # VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  # validates :email, presence: true, length: { maximum: 255 },
+  #                   format: { with: VALID_EMAIL_REGEX },
+  #                   uniqueness: { case_sensitive: false }
+
+
+  before_save { self.email = email.downcase }
+
+  validates :first_name, presence: true, length: { maximum: 50 }
+  validates :last_name, presence: true, length: { maximum: 50 }
+
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, length: { maximum: 255 },
+                    format: { with: VALID_EMAIL_REGEX },
+                    uniqueness: { case_sensitive: false }
+
+end
