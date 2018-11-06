fe918b0eb6d2f08b6b32c38107a187ff7b3a18f3
stephan-dev <stephan-dev@outlook.fr>
2018-11-06 01:08:40 +0100

    truc

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,23 @@
 class UsersController < ApplicationController
 	def new
-		#@user = User.new
+		@user = User.new
   end
 
   def show
   	@user = User.find(params[:id])
+  end
+
+  def create
+  	user = params[:user]
+
+    @user = User.new(first_name: user[:first_name], last_name: user[:last_name], email: user[:email])
+    if @user.save
+      flash[:success] = "Bienvenue au club sandwich!"
+      redirect_to @user
+    else
+      render 'new'
+    end
+
+
   end
 end
